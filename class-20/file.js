d7fd4b226f7b99f4a51e5b42f73763ecf679cf98
Irfan Ugljanin <irfanikresoft@gmail.com>
2022-04-12 18:19:12 +0200

    dsa

---
+++ class-20/file.js
@@ -0,0 +1,60 @@
+// var grade = 89;
+// if (grade < 55) {
+//   console.log("vasa ocena je 5");
+// } else if (grade > 55 && grade < 65)
+// console.log("vasa ocena je 6");
+// } else if (grade > 65 && grade < 75) {
+// console.log("vasa ocena je 7");
+// } else if (grade > 75 && grade < 85) {
+// console.log("vasa ocena je 8");
+// } else if (grade > 85 && grade < 95) {
+// console.log("vasa ocena je 9");
+// } if (grade > 95 && grade <= 100) {
+//     console.log('vasa ocena je 10')
+// }
+
+// var a = 20;
+// var b = 30;
+// var c = 25;
+
+// if (a > b) {
+//   if (a > c) {
+//     console.log(a);
+//   } else {
+//     console.log(c);
+//   }
+//   console.log(a);
+// } else {
+//   if (b > c) {
+//     console.log(b);
+//   } else {
+//     console.log(c);
+//   }
+// }
+
+// var array = [1, 12, 123, 432];
+
+// array.push("str");
+// array.pop();
+
+// console.log(array[3]);
+
+// var array = [1, 2, 3, 4, 5, 6];
+// var max = array[0];
+// var min = array[0];
+// for (var i = 0; i < array.length; i++) {
+//   if (max < array[i]) {
+//     max = array;
+//   }
+//   if (min > array[i]) {
+//     min = array;
+//   }
+// }
+
+var array1 = ["a", "b", "c", "d"];
+var array2 = ["a", "b", "f"];
+
+var uncommon = [];
+for (let index = 0; index < array.length; index++) {
+  const element = array1[index];
+}
---
+++ class-24/file.js
@@ -0,0 +1,58 @@
+// function concatArrays(array1, array2) {
+//   if (Array.isArray(array1) && Array.isArray(array2)) {
+//     return array1.concat(array2);
+//   }
+//   return undefined;
+// }
+
+// console.log(concatArrays([1, 2, 3], ["a", "b"]));
+
+// var commonLetters (a, b) {
+//     var common = [];
+//     for (let index1 = 0; index1 < a.length; index1 ++) {
+//         for ( let index2 = 0; index2 < b.length; index2++) {
+//         if (a[index1] === b[index2]) {
+//            common.push(a[index1])
+//         }
+//     }
+// }
+
+// console.log(commonLetters('abc', 'abds'))
+
+// var a = 10;
+
+// switch (a) {
+//   case 10:
+//     console.log("this is 10");
+//     break;
+
+//   case 20:
+//     console.log("this is 20");
+//     break;
+
+//   default:
+//     console.log("default");
+//     break;
+// }
+
+["red", "green", "blue", "white", "black"];
+
+function getHexColor(color) {
+  switch (color) {
+    case "red":
+      return "#FF00";
+
+    case "green":
+      return "#00ff";
+
+    case "blue":
+      return "0000ff";
+
+    case "white":
+      return "fff";
+
+    case "black":
+      return "000";
+  }
+}
+console.log(getHexColor("red"));
