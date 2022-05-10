b2a36b2c8157d47343ee235c9e73e7b1c06082bd
Irfan Ugljanin <irfanikresoft@gmail.com>
2022-05-10 18:46:08 +0200

    callbacks

---
+++ class-28/script.js
@@ -0,0 +1,39 @@
+function sum(a, b) {
+  return a + b;
+}
+
+//USING IMPORT AND EXPORT KEYWORDS
+//EXAMPLE EXPORTING FUNCTION 'SUM'
+export { sum };
+
+//USE IMPORT 
+///import {sum} from //./file name
+
+const closure = (array) => {
+  return () => {
+    return array.length > 0 ? true : false
+  }
+}
+
+const array1 = [123,23,32,3]
+const array2 = [12]
+
+const isEmpty1 = closure(array1);
+const isEmpty2 = closure(array2);
+
+//CALLBACK
+const logger 
+
+const sum = (a,b,callback) => {
+  if(callback) {
+    callback(a+b)
+  }
+  return a+b
+}
+
+sum(10, 20, (value) => console.log(value))
+
+const fn = () => {
+  console.log('hi')
+}
+setTimeout(fn, 1000)
