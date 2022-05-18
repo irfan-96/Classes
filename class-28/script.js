749b5988dffe8847f2d77ae78fd2a387daaf6a73
Irfan Ugljanin <irfanikresoft@gmail.com>
2022-05-18 14:43:05 +0200

    fetching

--- class-28/script.js
+++ class-28/script.js
@@ -1,39 +1,59 @@
-function sum(a, b) {
-  return a + b;
-}
+// // function sum(a, b) {
+// //   return a + b;
+// // }
 
-//USING IMPORT AND EXPORT KEYWORDS
-//EXAMPLE EXPORTING FUNCTION 'SUM'
-export { sum };
+// // //USING IMPORT AND EXPORT KEYWORDS
+// // //EXAMPLE EXPORTING FUNCTION 'SUM'
+// // export { sum };
 
-//USE IMPORT 
-///import {sum} from //./file name
+// // //USE IMPORT 
+// // ///import {sum} from //./file name
 
-const closure = (array) => {
-  return () => {
-    return array.length > 0 ? true : false
-  }
-}
+// // const closure = (array) => {
+// //   return () => {
+// //     return array.length > 0 ? true : false
+// //   }
+// // }
 
-const array1 = [123,23,32,3]
-const array2 = [12]
+// // const array1 = [123,23,32,3]
+// // const array2 = [12]
 
-const isEmpty1 = closure(array1);
-const isEmpty2 = closure(array2);
+// // const isEmpty1 = closure(array1);
+// // const isEmpty2 = closure(array2);
 
-//CALLBACK
-const logger 
+// // //CALLBACK
+// // const logger 
 
-const sum = (a,b,callback) => {
-  if(callback) {
-    callback(a+b)
-  }
-  return a+b
-}
+// // const sum = (a,b,callback) => {
+// //   if(callback) {
+// //     callback(a+b)
+// //   }
+// //   return a+b
+// // }
 
-sum(10, 20, (value) => console.log(value))
+// // sum(10, 20, (value) => console.log(value))
 
-const fn = () => {
-  console.log('hi')
-}
-setTimeout(fn, 1000)
+
+// //////////////////////
+// //SETITMEOUT
+// const fn = () => {
+//   console.log('hi')
+// }
+// setTimeout(fn, 1000)
+
+// //PROMISE
+// const promise = new Promise((ressolve,reject) => {
+//   const random = Math.random() * 1000;
+//   setTimeout(() => {
+//     if(random > 500) {
+//       resolve (random);
+//     } else {
+//       reject(random);
+//     }
+//   }, random )
+// });
+
+// fetch('')
+
+
+
