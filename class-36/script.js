1ff1252f65e3162a8552f24c880c4033e3f0b518
Irfan Ugljanin <irfanikresoft@gmail.com>
2022-05-24 20:03:28 +0200

    map and reduce

--- class-36/script.js
+++ class-36/script.js
@@ -1,3 +1,4 @@
+/*
 const users = [
   { id: 1, name: "Irfan", age: 25 },
   { id: 2, name: "Nikola", age: 19 },
@@ -45,3 +46,78 @@ const foundUsers = users.newFind((user, index) => {
 // });
 
 console.log(foundUsers);
+*/
+// const array = [
+//   {
+//     id: 1,
+//     name: "Suad",
+//     address: { street: "Hotkovo", number: 22 },
+//   },
+//   {
+//     id: 2,
+//     name: "Irfan",
+//     address: { street: "Banja", number: 22 },
+//   },
+//   {
+//     id: 3,
+//     name: "Ramiz",
+//     address: { street: "Potok", number: 22 },
+//   },
+//   {
+//     id: 4,
+//     name: "Ahmed",
+//     address: { street: "Hadzet", number: 22 },
+//   },
+
+//   {
+//     id: 5,
+//     name: "Samil",
+//     address: { street: "Rasadnik", number: 22 },
+//   },
+// ];
+
+// // const newArray2 = array
+// //   .filter((user) => user.name[0] === "S")
+// //   .map((user) => user.name);
+
+// console.log(newArray);
+
+// Array.prototype.newMap = function (callback) {
+//   let arr = [];
+//   for (let index = 0; index < this.length; index++) {
+//     const element = this[index];
+//     arr.push(callback(element, index, this));
+//   }
+//   return arr;
+// };
+
+// const newArray = array
+//   .filter((user) => user.address.number > 25)
+//   .map((user) => user.adress);
+
+// const newArray2 = array.reduce((previous, current, index, array) => {
+//   if (current.address.number > 25) {
+//     previous.push(value.address);
+//   }
+//   return previous;
+// }, []);
+
+Array.prototype.newReduce = function (callback, initialValue) {
+  let result = initialValue ? initialValue : this[0];
+
+  for (let index = 0; index < this.length; index++) {
+    const element = this[index];
+
+  if (initialValue) {
+    result = callback(result, element, index, this);
+    continue;
+  }
+   if(!initialValue && index === 0 ) {
+     continue;
+  }
+  result = callback(result, element, index, this);
+
+  return result;
+};
+
+[1, 2, 3, 4].newReduce((result, value) => result + value);
